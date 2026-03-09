98752cf037222ab0e5c38056ef32854dccd659c4
Andrei Jay <andreijay05@gmail.com>
2026-03-09 21:31:19 +0800

    Updates

---
+++ game/assets/tiles/greenzone_tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="greenzone_tileset" tilewidth="32" tileheight="32" tilecount="96" columns="12">
+ <image source="greenzone_tileset.png" width="384" height="256"/>
+</tileset>
